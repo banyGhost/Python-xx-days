7fc2107a10387123565773aec4273a66dbfc6824
任梁樊 <renliangfan@192.168.101.105>
2019-05-09 17:21:24 +0800

    feat: add fib

--- day-01/fib.py
+++ day-01/fib.py
@@ -5,4 +5,15 @@ __author__name = 'Bert'
 
 
 def fib(n):
-    pass
+    L = []
+    while True:
+        if len(L) == 0 or len(L) == 1:
+            L.append(1)
+        else:
+            L.append(L[-1] + L[-2])
+        if len(L) == n:
+            break
+    return L
+
+
+print(fib(10))
